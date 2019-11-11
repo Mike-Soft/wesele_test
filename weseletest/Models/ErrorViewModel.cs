65d7f44edfff3ae1dfd8a932c237e8faa03d8239
38444 <38444@studenci.pwsz.legnica.edu.pl>
2019-11-11 21:54:06 +0100

    dodany pełny projekt

---
+++ weseletest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace weseletest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
