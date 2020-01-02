0d6189d82036f0afa054afd71c1189ee70ba12ff
kamil109111@gmail.com <kamil109111@gmail.com>
2020-01-02 14:10:21 +0100

    Wypełnienie bazy klientów

---
+++ weseletest/Data/Migrations/20200102125144_dod.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace weseletest.Data.Migrations
+{
+    public partial class dod : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+            migrationBuilder.Sql(@"SET IDENTITY_INSERT [dbo].[Customer] ON
+                                 INSERT INTO [dbo].[Customer] ([Id], [Name], [LastName]) VALUES (3, N'Jan', N'Nowak')
+                                 INSERT INTO [dbo].[Customer] ([Id], [Name], [LastName]) VALUES (4, N'Kamil', N'Lachowicz')
+                                 SET IDENTITY_INSERT [dbo].[Customer] OFF");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
